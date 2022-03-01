06a3f624c45b6a06a6bf6ef7315b8bdcbaf00025
yangtao27 <yangtao27@MacintoshdeMacBook-Pro.local>
2022-03-01 19:05:01 +0800

    simplify

--- writer/writer.go
+++ writer/writer.go
@@ -1,14 +1,10 @@
 package writer
 
-import "errors"
-
 const (
 	BufferFlushSeconds = 1
 	FileBufferSize     = 4096
 	ChanBufferSize     = 10000
 )
-
-var ErrFileWriter = errors.New("create fileWriter err")
 
 // 声明日志写入器接口
 type LogWriter interface {
